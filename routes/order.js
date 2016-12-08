01c1ef489d4f07ee4f1eef36ca0cbd6e28a5bcbc
Cole Albon <cole.albon@gmail.com>
2016-12-07 20:16:21 -0700

    install fake json (still empty) servers for acme and ranier

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,60 @@
 import { Router } from 'express';
 const bodyParser = require('body-parser')
-const objectAssign = require('object-assign');
+//const objectAssign = require('object-assign');
 const router = new Router();
-
 var jsonParser = bodyParser.json()
+
+function validateOrder (order) {;
+    let orderObj = order;
+    try {
+        orderObj.status = 'fail'
+        // refactor: could/should guard with lense or class here
+        // except I like to say which missing param
+        if (!orderObj.make) {
+            orderObj.reason = "missing attribute: make"
+            return orderObj
+        }
+        if (!orderObj.model) {
+            orderObj.reason = "missing attribute: model"
+            return orderObj
+        }
+        if (!orderObj.package) {
+            orderObj.reason = "missing attribute: package"
+            return orderObj
+        }
+        if (!orderObj.customer) {
+            orderObj.reason = "missing attribute: customer"
+            return orderObj
+        }
+        if (!orderObj.customer.id) {
+            orderObj.reason = "missing attribute: customer.id"
+            return orderObj
+        }
+        if (!orderObj.customer.shipto) {
+            orderObj.reason = "missing attribute: customer.shipto"
+            return orderObj
+        }
+        if (orderObj.customer.shipto === 'siberia') {
+            orderObj.reason = "negatron, siberia is too expensive"
+            return orderObj
+        }
+        orderObj.status = 'success';
+        return orderObj;
+    } catch (err) {
+        next(err);
+    }
+}
+
+function sourceOrder (order) {
+    let orderObj = order;
+    orderObj.status = 'fail'
+    try {
+        orderObj.status = 'success';
+        return orderObj;
+    } catch (err) {
+        next(err);
+    }
+}
 
 router.get('/', async (req, res, next) => {
   try {
@@ -13,46 +64,20 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-// POST /api/users gets JSON bodies
 router.post('/', jsonParser, async (req, res, next) => {
     if (!req.body) return res.sendStatus(400)
-    try {
-        let orderObj = objectAssign(req.body);
-        // refactor: could/should guard with lense or class here
-        // except I like to say which missing param
-        if (!orderObj.make) {
-            res.status(400).send("missing attribute: make");
-            return
-        }
-        if (!orderObj.model) {
-            res.status(400).send("missing attribute: model");
-            return
-        }
-        if (!orderObj.package) {
-            res.status(400).send("missing attribute: package");
-            return
-        }
-        if (!orderObj.customer) {
-            res.status(400).send("missing attribute: customer");
-            return
-        }
-        if (!orderObj.customer.id) {
-            res.status(400).send("missing attribute: customer.id");
-            return
-        }
-        if (!orderObj.customer.shipto) {
-            res.status(400).send("missing attribute: customer.shipto");
-            return
-        }
-        if (orderObj.customer.shipto === 'siberia') {
-            res.status(400).send("negatron, siberia is too expensive");
-            return
-        }
-        orderObj.status = 'success';
-        res.status(200).json(orderObj)
-    } catch (err) {
-        next(err);
+    let validatedOrder = await validateOrder(req.body);
+    if (validatedOrder.status === 'fail') {
+        res.status(400).send(validatedOrder.reason);
+        return
     }
+    let sourcedOrder = await sourceOrder(validatedOrder);
+    if (sourcedOrder.status === 'fail') {
+        res.status(400).send(sourcedOrder.reason);
+        return
+    }
+    res.status(200).send(sourcedOrder);
+    return
 })
 
 export default router;
